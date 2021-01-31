7e41225031c15700cdff82022d0a123a394b7388
vuki656 <off.vukovic@gmail.com>
2021-01-31 21:54:52 +0100

    feature: added anydo

--- src/lib/websites/websites.ts
+++ src/lib/websites/websites.ts
@@ -12,7 +12,7 @@ export const websites: WebsiteType[] = [
     {
         bannerPath: '/websites/banners/segment.png',
         color: '#5cc09b',
-        description: 'The leading Customer Data Platform',
+        description: 'Customer Data Platform',
         faviconPath: '/websites/favicons/segment.png',
         name: 'Segment',
         palette: ['#5cc09b', '#211e32', '#faf8fd'],
@@ -21,7 +21,7 @@ export const websites: WebsiteType[] = [
     {
         bannerPath: '/websites/banners/strapi.png',
         color: '#8c4bff',
-        description: 'Leading open-source headless CMS.',
+        description: 'Open-source headless CMS',
         faviconPath: '/websites/favicons/strapi.png',
         name: 'Strapi',
         palette: ['#1c1b7e', '#8c4bff', '#fee895', '#fd798c', '#80ebff'],
@@ -30,7 +30,7 @@ export const websites: WebsiteType[] = [
     {
         bannerPath: '/websites/banners/expo.png',
         color: '#a852dc',
-        description: 'Build one project that runs natively on all your users\' devices',
+        description: 'Open-source platform for building cross-platform apps',
         faviconPath: '/websites/favicons/expo.png',
         name: 'Expo',
         palette: ['#191936', '#0086fc', '#d957d9', '#7f6df3', '#4630eb'],
@@ -39,7 +39,7 @@ export const websites: WebsiteType[] = [
     {
         bannerPath: '/websites/banners/canny.png',
         color: '#969dfa',
-        description: 'Build better products with customer feedback',
+        description: 'Customer feedback platform',
         faviconPath: '/websites/favicons/canny.png',
         name: 'Canny',
         palette: ['#6dd345', '#f8f8fa', '#969dfa', '#4753c6', '#3d498d'],
@@ -48,10 +48,19 @@ export const websites: WebsiteType[] = [
     {
         bannerPath: '/websites/banners/shuffle.png',
         color: '#3758f9',
-        description: 'Create templates in minutes',
+        description: 'Online UI Editor',
         faviconPath: '/websites/favicons/shuffle.png',
         name: 'Shuffle',
         palette: ['#3758f9', '#8d1afc', '#00b0ff', '#00d1b2', '#18b9d1', '#ff8577', '#ffc100', '#f6faff'],
-        url: 'https://expo.io/',
+        url: 'https://shuffle.dev/',
+    },
+    {
+        bannerPath: '/websites/banners/anydo.png',
+        color: '#0083ff',
+        description: 'Todo App',
+        faviconPath: '/websites/favicons/anydo.png',
+        name: 'Any.do',
+        palette: ['#0083ff', '#ebebeb', '#8c23aa', '#6b66cf', '#ffae26', '#f60350'],
+        url: 'https://www.any.do/',
     },
 ]
